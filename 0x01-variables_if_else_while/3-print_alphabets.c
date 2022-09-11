fc8d2cf7a7663d09fa6a18c6396c4a07db17d605
Mayor-king <maylaw203@gmail.com>
2022-09-11 09:03:07 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,2 +1,23 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
+int main(void)
+{
+	char ma;
+	char sir;
+
+	for (ma = 'a'; ma <= 'z'; ma++)
+	{
+		putchar(ma);
+	}
+	for (sir = 'A'; sir <= 'Z'; sir++)
+	{
+		putchar(sir);
+	}
+	putchar('\n');
+		return (0);
+}
