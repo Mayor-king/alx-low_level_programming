20d891600f79b14e7b2c5a07c7589b7b7ffbb216
Mayor-king <maylaw203@gmail.com>
2022-09-12 08:57:59 -0700

    Printing single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio>
+/**
+ * main - print base 10 single digit
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
