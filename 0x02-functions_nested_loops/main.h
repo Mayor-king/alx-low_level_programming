c4d5fa867be050ff0bd5de0d49027e40020fcb3a
Mayor-king <maylaw203@gmail.com>
2022-09-13 08:19:10 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+int _putchar(char x);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int s);
+int _isalpha(int s);
+int print_sign(int s);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int s);
+void print_times_table(int s);
