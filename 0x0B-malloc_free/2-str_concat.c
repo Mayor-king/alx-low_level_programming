ae8da589d0184c3b43ff8dca20d1ffb1b48efac2
Mayor-king <maylaw203@gmail.com>
2022-10-05 10:29:48 -0700

    concantenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -31,7 +31,8 @@ char *str_concat(char *s1, char *s2)
 	{
 		ch2++;
 	}
-	strNew = malloc(sizeof(char) * (ch1 + ch2 + 1));
+	ch2++
+	strNew = malloc(sizeof(char) * (ch1 + ch2));
 	if (strNew == NULL)
 	{
 		return (NULL);
