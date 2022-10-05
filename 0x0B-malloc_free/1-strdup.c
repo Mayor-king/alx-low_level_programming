89cb5c7712813fca25d4bca6012d5ae8cf1163e5
Mayor-king <maylaw203@gmail.com>
2022-10-05 06:51:50 -0700

    duplicating string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - duplicate a string
+ * @str: char param
+ * Return: pointer of string
+ */
+char *_strdup(char *str);
+{
+	char *strDup;
+	unsigned int a, b;
+
+	a = 0;
+	b = 0;
+
+	if (str = NULL)
+	{
+		return (NULL);
+	}
+	while (a != '\0')
+	{
+		a++
+	}
+	strDup = malloc(sizeof(str) * (a + 1));
+
+	if (strDup = NULL)
+	{
+		return (NULL);
+	}
+	while (b < a)
+	{
+		strDup[b] = str[b];
+		b++
+	}
+	strDup[b] = '\0';
+
+	return (strDup);
+}
