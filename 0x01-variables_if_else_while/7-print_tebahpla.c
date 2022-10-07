29a557c97f839dfd0c5d52b9555b1f67b5f66e39
Mayor-king <maylaw203@gmail.com>
2022-10-07 08:55:02 -0700

    smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point
  *
- * Return: always 0 
+ * Return: always 0
  */
 int main(void)
 {
@@ -12,8 +12,8 @@ int main(void)
 	while (c <= 'a')
 	{
 		putchar(c);
-		c--;
 	}
+	c--;
 	putchar('\n');
 	return (0);
 }
