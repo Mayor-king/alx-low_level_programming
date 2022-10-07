a3427692ea4661061d6ca5b2880481e7ca034b84
Mayor-king <maylaw203@gmail.com>
2022-10-07 15:08:53 -0700

    different combinationfor 2 digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of two digits
+ *
+ * Return: success
+ */
+int main(void)
+{
+	int c;
+	int h;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (h = '0'; h < '10'; h++)
+		{
+			putchar(c);
+			putchar(h);
+
+			if ((c != '9') || (c = '9' && h != '9'))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\0');
+
+	return (0);
+}
+
