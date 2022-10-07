8a080b551e876f6bba89ccada16d41cf20b2e0f7
Mayor-king <maylaw203@gmail.com>
2022-10-07 09:45:28 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	char ch;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n')
+}
