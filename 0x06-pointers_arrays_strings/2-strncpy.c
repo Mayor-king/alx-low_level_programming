95ccce66e50b6497e708079b2ed069a87ad1f303
Mayor-king <maylaw203@gmail.com>
2022-10-17 10:25:25 -0700

    copy string into a variable

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,14 +10,14 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (*(src + i) != '\0')
+	while (*(src + i) != '\0' && i < n)
 	{
 		*(dest + i) = src[i];
 		i++;
 	}
 	while (i < n)
 	{
-		if (src[i] == '\0')
+		if (dest[i] == '\0')
 			break;
 		i++;
 	}
