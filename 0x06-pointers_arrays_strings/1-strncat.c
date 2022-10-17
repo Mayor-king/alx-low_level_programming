be3307ca758b943d5fa5e99ba50c01bb3a2f5eee
Mayor-king <maylaw203@gmail.com>
2022-10-17 09:26:16 -0700

    concatenating n strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * @dest: string to append to
+ * @src: string that get appended
+ * @n: to number in string that get appended
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int con = 0, cat = 0;
+	while (*(dest + con) != '\0')
+	{
+		con++;
+	}
+	while (cat < 0)
+	{
+		*(dest + con) = *(src + cat);
+		if (*(src + cat) = '\0')
+			break;
+		con++;
+		cat++;
+	}
+	return (dest);
+}
