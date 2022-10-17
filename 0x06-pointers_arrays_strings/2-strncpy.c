6860c63aecd7979659a88d064fe8bc21ea7f811b
Mayor-king <maylaw203@gmail.com>
2022-10-17 10:07:10 -0700

    copy string into a variable

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - copying string to a destination
+ * @dest: string copying into
+ * @src: string to be copied
+ * @n: number of byte(s) of src
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (*(dest + i) != '\0')
+	{
+		i++;
+	}
+	while (i < n)
+	{
+		*(dest + i) = src[i];
+		if (src[i] == '\0')
+			break;
+		i++
+	}
+	return (dest);
+}
