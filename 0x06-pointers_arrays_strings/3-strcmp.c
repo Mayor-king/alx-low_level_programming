e3ad35f3c41ecbe9f150eba207af402d347fb3e1
Mayor-king <maylaw203@gmail.com>
2022-10-18 02:09:05 -0700

    compared two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,14 +9,14 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	for (;s1[i] != '\0' || s2[i] = '\0'; i++)
+	while (i == 0)
 	{
-		if (s1[i] != s2[i])
+		if (s1[i] == '\0' && s2[i])
 		{
-			return (s1[i] - s2[i]);
-		
+			break;		
 		}
+		i++;
 
 	}
-	return (0);
+	return (s1[i] - s2[i]);
 }
