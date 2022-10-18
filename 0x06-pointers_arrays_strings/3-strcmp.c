146e1706c3bfef71ad22d878ccdee8f6d959c8db
Mayor-king <maylaw203@gmail.com>
2022-10-18 01:52:31 -0700

    compared two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcmp - compare 2 strings
+ * @s1: string comparing another
+ * @s2: string that will be compared
+ * Return: difference
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' || s2[i] = '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
